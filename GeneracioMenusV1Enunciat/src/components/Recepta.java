71b7184958c7adedabeb1d23a89391ea9a08d6d6
Ricard Pinzón <riki_net69@hotmail.com>
2018-10-04 14:26:46 +0200

    Add files via upload

--- GeneracioMenusV1Enunciat/src/components/Recepta.java
+++ GeneracioMenusV1Enunciat/src/components/Recepta.java
@@ -18,12 +18,12 @@ public class Recepta {
 
     private String codi;
     private String nom;
-    private Aliment[] ingredients;
-    private int posicioIngredients; //Possició actual buida del vector ingredients
+    private Aliment[] ingredients = new Aliment[25];
+    private int posicioIngredients = 0; //Possició actual buida del vector ingredients
     private String elaboracio;
     private String tipus;
     private double calories;
-    private boolean assignada;
+    private boolean assignada = false;
 
     /*
      CONSTRUCTOR
@@ -37,7 +37,6 @@ public class Recepta {
      - assignada s'ha d'inicialitzar a false, ja que cuan es crea una recepta no està
      assignada a cap menú.
      */
-    
     public Recepta(String cd, String nm, String elab, String tip, double cal) {
         codi = cd;
         nom = nm;
@@ -56,13 +55,13 @@ public class Recepta {
     public void setNom(String nm) {
         nom = nm;
     }
-    
-    public void setIngredients(){
-         ingredients = new Aliment[25]; 
+
+    public void setIngredients(Aliment[] alim) {
+        ingredients = alim;
     }
-    
-    public void setPosicioIngredients(){
-        posicioIngredients = 0;
+
+    public void setPosicioIngredients() {
+        posicioIngredients++;
     }
 
     public void setElaboracio(String elab) {
@@ -76,11 +75,11 @@ public class Recepta {
     public void setCalories(double cal) {
         calories = cal;
     }
-    
-    public void setAssignada(){
-        assignada = false;
+
+    public void setAssignada(boolean as) {
+        assignada = as;
     }
-    
+
     public String getCodi() {
         return codi;
     }
@@ -100,8 +99,6 @@ public class Recepta {
     public double getCalories() {
         return calories;
     }
-    
-    
 
     /*
      Paràmetres: cap
@@ -112,7 +109,7 @@ public class Recepta {
      - En el cas del tipus, l'usuari ha d'introduir 1, si es tracta d'un primer plat, 2
      si es tracta d'un segon i P si són unes postres. Heu de comprovar que la dada
      introduida per l'usuari és la correcta (1, 2 o P) si no li mostrarem el missatge
-      i tornarem a demanar-li que introdueixi la dada.
+     "\nLa dada introduida no és vàlida" i tornarem a demanr-li que introdueixi la dada.
      Això ho farem fins que introdueixi la dada correcta.
     
      - També heu de tenir en compte que l'elaboració, no estarà formada per una única
@@ -139,21 +136,21 @@ public class Recepta {
 
         System.out.println("Tipus: 1:Primer plat, 2:segon plat, P:postre");
         tipus = DADES.nextLine();
-        
+
         while (!tipus.equals("1") || !tipus.equals("2") || !tipus.equals("P")) {
 
             System.out.println("La dada introduida no és vàlida");
-            
+
             System.out.println("Torna a introduir la dada.");
             tipus = DADES.nextLine();
         }
 
         System.out.println("Calories: ");
         calories = DADES.nextDouble();
-        
+
         Recepta recepta = new Recepta(codi, nom, elaboracio, tipus, calories);
 
-        return recepta;
+        return recepta; //Retorna l'objecte actual.
     }
 
     /*
@@ -162,39 +159,52 @@ public class Recepta {
      - Demanar a l'usuari que introdueixi les noves dades de l'objecte actual
      i modificar els atributs corresponents d'aquest objecte. En aquest cas no
      es pot modificar l'atribut ingredients, ni l'atribut assignada.
-    
      - En el cas del tipus, l'usuari ha d'introduir 1, si es tracta d'un primer plat, 2
      si es tracta d'un segon i P si són unes postres. Heu de comprovar que la dada
      introduida per l'usuari és la correcta (1, 2 o P) si no tornarem a demanr-li 
      que introdueixi la dada. Això ho farem fins que introdueixi la dada correcta.
-     
-    - Li heu de mostrar a l'usuari el valor actual dels atributs de l'objecte
+     - Li heu de mostrar a l'usuari el valor actual dels atributs de l'objecte
      actual, abans de modificar-los
      Retorn: cap
      */
     public void modificarRecepta() {
-        
-        
-        System.out.println("introdueix les noves dades de l'objecte actual:");
-        
-        System.out.println("codi:");
-        
-        
-        System.out.println("Nom:");
-        
-        
-        
-        
-        
-        
-        
-        
+        /*Quan el metode es public i volem editar o accedir als atributs del objecte actual, 
+        s'ha de editar el atribut directament dins del metode.*/
+
+ /*TAMBE ES CORRECTE*/
+        System.out.println("Les dades actuals de la recepta son: ");
+        System.out.println("codi" + getCodi());
+        System.out.println("Nom" + getNom());
+        System.out.println("Elaboracio" + getElaboracio());
+        System.out.println("Tipus" + getTipus());
+        System.out.println("Calories" + getCalories());
+
+        System.out.println("Introdueix les dades a modificar de l'objecte actual: ");
+
+        System.out.println("Codi: ");
+        codi = DADES.nextLine();
+
+        System.out.println("Nom: ");
+        nom = DADES.nextLine();
+
+        System.out.println("Elaboracio: ");
+        elaboracio = DADES.nextLine();
+
+        System.out.println("Tipus: 1:Primer plat, 2:segon plat, P:postre");
+        tipus = DADES.nextLine();
+
+        while (!tipus.equals("1") || !tipus.equals("2") || !tipus.equals("P")) {
+
+            System.out.println("La dada introduida no és vàlida");
+
+            System.out.println("Torna a introduir la dada.");
+            tipus = DADES.nextLine();
+        }
+
+        System.out.println("Calories: ");
+        calories = DADES.nextDouble();
     }
 
-    
-    
-    
-    
     public void mostrarRecepta() {
         System.out.println("\nLes dades de la recepta amb codi " + codi + " són:");
         System.out.println("\nNom:" + nom);
@@ -213,16 +223,34 @@ public class Recepta {
  /*
      Paràmetres: cap
      Accions:
+    
      - afegeix un nou ingredient al vector d'ingredients d'aquesta recepta (l'objecte actual),
      si aquest encara no s'ha afegit, fent servir el mètode de la classe Ingredients pertinent,    
      i actualitza la posició del vector d'ingredients.
+    
      - heu de fer servir el mètode escaient d'aquesta classe per comprovar si l'ingredient
      ja s'ha afegit o no.
+    
      - Si l'ingredient ja s'habia afegit, no s'afegirà de nou i li mostrarem a l'usuari
      el missatge "\nL'ingredient ja existeix".
      Retorn: cap
      */
     public void afegirIngredient() {
+
+        /*
+        ingredients=new Aliment[posicioIngredient];
+        posicioIngredients++;
+         */
+ /*
+        Aliment ingredient = new Aliment();
+        ingredients[posicioIngredients]=ingredient;
+         */
+        if (seleccionarIngredient(codi) == posicioIngredients) {
+            System.out.println("L'ingredient ja existeix");
+        } else {
+            ingredients[posicioIngredients] = Aliment.nouAliment();
+            posicioIngredients++;
+        }
 
     }
 
